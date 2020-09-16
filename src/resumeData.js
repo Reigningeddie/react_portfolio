f562818a1d5f5f1a44519a377631d8a5939ad657
Eduardo Aguilar <eaguilar92105@yahoo.com>
2020-09-15 17:53:01 -0700

    react portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,96 @@
+let resumeData = {
+    "imagebaseurl":"https://reigningeddie.github.io/",
+    "name": "Eduardo Aguilar",
+    "role": "Frontend Developer",
+    "linkedinId":"Reigningeddie",
+    "skypeid": "ReigningEddie",
+    "roleDescription": "I like dabbling in various parts of web development and experimenting with new technologies, go out golfing or simply play games on my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/reigningeddie/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/reigningeddie",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I am currently in the trilogy program with UCSD finishing up a full stack web developer certification. I want to dive deeper into Machine Learning and programing. I find the subject fascinating, I have found myself obsessing over web developing constantly looking for new and exciting technologies.",
+    "address":"San Diego, CA",
+    "website":"https://reigningeddie.react_portfolio",
+    "education":[
+      {
+        "UniversityName":"UCSD",
+        "specialization":"web development",
+        "MonthOfPassing":"September",
+        "YearOfPassing":"2020",
+      },
+      {
+        "UniversityName":"Mesa College",
+        "specialization":"english",
+        "MonthOfPassing":"-",
+        "YearOfPassing":"-",
+        "Achievements":"-"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"In N Out",
+        "specialization":"Cook",
+        "MonthOfLeaving":"Current",
+        "YearOfLeaving":"2020",
+        "Achievements":"-"
+      },
+    ],
+    "skillsDescription":"Web Development",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Node.js"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"PasswordGenerator",
+        "description":"a generator for password, which you can pass in certain parameters",
+        "imgurl":"../public/images/portfolio/password-generator.jpg"
+      },
+      {
+        "name":"BeHealth",
+        "description":"a form you can fill out to figure out how many calories you should be consumeing, then passes through 3 recipes that fall under set parameters.",
+        "imgurl":"images/portfolio/be-healthy.jpg"
+      },
+      {
+        "name":"ForgetMeNot",
+        "description":"a to do list where you can log in, and we implemented a current COVID tracking api where you can see how many people are affected in that state.",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"Artsy",
+        "description":"a pictionary application where you can chat while playing pictionary with as many friends you want online, or on your phones in person.",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
